1de0852ac21f49d63f86a7840a5c2e634fafed5a
Tomten <alle090@hotmail.com>
2026-09-14 12:57:39 +0200

    starting to learn Textual.

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import base.static_variables
 
+from textual.app import App
+from textual.widgets import Footer, Header, Static
+
+class DEVICEFEED():
+    pass
+
+class MAINWINDOW(Static):
+    """börje"""
+    def compose(self):
+        yield DEVICEFEED()
+
+
+
+class KitApp(App):
+    BINDINGS = [
+        ("d", "toggle_dark_mode", "Toggle dark mode")
+    ]
+
+    def compose(self):
+        ## What is this app composed of
+        self.title = "Kistmet in terminal"
+        yield Header(show_clock=True)
+        yield Footer()
+        yield MAINWINDOW()
+    
+    def action_toggle_dark_mode(self):
+        self.theme = "textual-dark" if self.theme == "textual-light" else "textual-light"
+
 
 
 def main():
-    pass
+    KitApp().run()
 
 
 
